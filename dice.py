366648ab13961a9b22b729766244674ef0c9a67d
tvfrahul <tvfrahul@gmail.com>
2018-08-04 23:38:28 +0530

    added dice.py

---
+++ dice.py
@@ -0,0 +1,16 @@
+import random
+while 1:
+ print("Hello, Lets play dice game")
+ play = input("Do you want to play, if yes Press Y").lower()
+ def dice() :
+    roll = random.randint(1,6)
+    return roll
+ if play == "y":
+    result = dice()
+    print("you got",result)
+ else:
+    print("Fuck off")
+    exit()
+
+
+
